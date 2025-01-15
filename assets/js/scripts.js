ab87123a6d760ece49e033fe5fdb2fb817b62dfa
Edite10 <budzilkoedite@gmail.com>
2025-01-15 17:48:57 +0000

    Make video facade and style it

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+function loadVideo() {
+    var facade = document.querySelector('.video-facade');
+    facade.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/HT9dtDSRDFA?autoplay=1&si=pjFVNy1UqYC9_OnG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+}
